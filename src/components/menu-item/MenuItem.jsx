853207a29ddd3bf6eb4beff445da1f4c1e6c9e18
Anthony Washington <awashington251@gmail.com>
2020-04-19 23:37:53 -0400

    add menu item background zoom+styling

--- src/components/menu-item/MenuItem.jsx
+++ src/components/menu-item/MenuItem.jsx
@@ -4,14 +4,15 @@ import "src/components/menu-item/menuitem.styles.scss";
 
 const MenuItem = ({ size, title, imageUrl }) => {
   return (
-    <div
-      style={{
-        backgroundImage: `url(${imageUrl})`,
-      }}
-      className={`${size} menu-item`}
-    >
+    <div className={`${size} menu-item`}>
+      <div
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+        className="background-image"
+      />
       <div className="item-content">
-        <h1 className="item-title">{title}</h1>
+        <h1 className="item-title">{title.toUpperCase()}</h1>
         <span className="item-subtitle">Shop Now</span>
       </div>
     </div>
