3687943c0f669fb670f82b24c332094ab8e7b7bd
Anthony Washington <awashington251@gmail.com>
2020-10-12 22:47:39 -0400

    add collection item add to cart button and styles enhancement

--- src/components/collection-item/CollectionItem.jsx
+++ src/components/collection-item/CollectionItem.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import FormButton from "src/components/button/FormButton";
+
 import "src/components/collection-item/collectionitem.styles.scss";
 
 export const CollectionItem = ({ name, imageUrl, price }) => (
@@ -14,5 +16,6 @@ export const CollectionItem = ({ name, imageUrl, price }) => (
       <span className="name">{name}</span>
       <span className="price">${price}</span>
     </div>
+    <FormButton label="ADD TO CART" customStyle="inverted" />
   </div>
 );
