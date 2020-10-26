080b95426628b4419fe5227da97ee25a9a84f94f
Anthony Washington <awashington251@gmail.com>
2020-10-25 22:17:43 -0400

    connect cart dropdown to redux and render cart items

--- src/components/cart-dropdown-menu/CartDropDownMenu.jsx
+++ src/components/cart-dropdown-menu/CartDropDownMenu.jsx
@@ -1,14 +1,22 @@
 import React from "react";
+import { connect } from "react-redux";
 
+import CartItem from "src/components/cart-item/CartItem";
 import FormButton from "src/components/button/FormButton";
 
 import "src/components/cart-dropdown-menu/cartdropdownmenu.styles.scss";
 
-const CartDropDownMenu = () => (
+const CartDropDownMenu = ({ cartItems }) => (
   <div className="cart-dropdown">
-    <div className="cart-items">This is the CartDropDownMenu!</div>
+    <div className="cart-items">
+      {cartItems.map((cartItem) => (
+        <CartItem key={cartItem.id} item={cartItem} />
+      ))}
+    </div>
     <FormButton label="GO TO CHECKOUT" />
   </div>
 );
 
-export default CartDropDownMenu;
+const mapStateToProps = ({ cart: { cartItems } }) => ({ cartItems });
+
+export default connect(mapStateToProps)(CartDropDownMenu);
