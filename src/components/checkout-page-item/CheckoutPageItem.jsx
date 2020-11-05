496a106088371e5a525e986f43bfd086a098aa28
Anthony Washington <awashington251@gmail.com>
2020-11-05 09:18:12 -0500

    component: Dispatch increase/decrease item cart quantity in checkout page

--- src/components/checkout-page-item/CheckoutPageItem.jsx
+++ src/components/checkout-page-item/CheckoutPageItem.jsx
@@ -2,11 +2,20 @@ import React from "react";
 
 import { connect } from "react-redux";
 
-import { removeItemFromCartAction } from "src/redux/cart/cartActions";
+import {
+  addItemToCartAction,
+  decreaseCartItemQuantityAction,
+  removeItemFromCartAction,
+} from "src/redux/cart/cartActions";
 
 import "src/components/checkout-page-item/checkoutpageitem.styles.scss";
 
-const CheckoutPageItem = ({ cartItem, dispatch }) => {
+const CheckoutPageItem = ({
+  cartItem,
+  removeItemFromCart,
+  increaseCartItemQuantity,
+  decreaseCartItemQuantity,
+}) => {
   return (
     <div className="checkout-page-item">
       <div className="item-image-container">
@@ -14,14 +23,24 @@ const CheckoutPageItem = ({ cartItem, dispatch }) => {
       </div>
       <div className="item-name">{cartItem.name}</div>
       <div className="item-quantity">
-        <div className="item-arrow">&#10094;</div>
+        <div
+          className="item-arrow"
+          onClick={() => decreaseCartItemQuantity(cartItem)}
+        >
+          &#10094;
+        </div>
         <span className="item-value">{cartItem.quantity}</span>
-        <div className="item-arrow">&#10095;</div>
+        <div
+          className="item-arrow"
+          onClick={() => increaseCartItemQuantity(cartItem)}
+        >
+          &#10095;
+        </div>
       </div>
       <div className="item-price">${cartItem.price}</div>
       <div
         className="remove-item-button"
-        onClick={() => dispatch(removeItemFromCartAction(cartItem))}
+        onClick={() => removeItemFromCart(cartItem)}
       >
         &#10005;
       </div>
@@ -29,4 +48,13 @@ const CheckoutPageItem = ({ cartItem, dispatch }) => {
   );
 };
 
-export default connect()(CheckoutPageItem);
+const mapDispatchToProps = (dispatch) => ({
+  removeItemFromCart: (cartItem) =>
+    dispatch(removeItemFromCartAction(cartItem)),
+  increaseCartItemQuantity: (cartItem) =>
+    dispatch(addItemToCartAction(cartItem)),
+  decreaseCartItemQuantity: (cartItem) =>
+    dispatch(decreaseCartItemQuantityAction(cartItem)),
+});
+
+export default connect(null, mapDispatchToProps)(CheckoutPageItem);
