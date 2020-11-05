b7cc6a124f80e2f000111197734f3c9d0ca9fca6
Anthony Washington <awashington251@gmail.com>
2020-11-05 07:57:38 -0500

    component: Dispatch remove item from cart in checkout page

--- src/components/checkout-page-item/CheckoutPageItem.jsx
+++ src/components/checkout-page-item/CheckoutPageItem.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 
+import { connect } from "react-redux";
+
+import { removeItemFromCartAction } from "src/redux/cart/cartActions";
+
 import "src/components/checkout-page-item/checkoutpageitem.styles.scss";
 
-const CheckoutPageItem = ({ cartItem }) => {
+const CheckoutPageItem = ({ cartItem, dispatch }) => {
   return (
     <div className="checkout-page-item">
       <div className="item-image-container">
@@ -11,9 +15,14 @@ const CheckoutPageItem = ({ cartItem }) => {
       <div className="item-name">{cartItem.name}</div>
       <div className="item-quantity">{cartItem.quantity}</div>
       <div className="item-price">${cartItem.price}</div>
-      <div className="remove-item-button">&#10005;</div>
+      <div
+        className="remove-item-button"
+        onClick={() => dispatch(removeItemFromCartAction(cartItem))}
+      >
+        &#10005;
+      </div>
     </div>
   );
 };
 
-export default CheckoutPageItem;
+export default connect()(CheckoutPageItem);
