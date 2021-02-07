10456200f0fb815a399adf30a7c448578a55b870
Anthony Washington <awashington251@gmail.com>
2021-02-07 18:14:07 -0500

    component: collection preview clickable titles show all items

--- src/components/collection-preview/CollectionPreview.jsx
+++ src/components/collection-preview/CollectionPreview.jsx
@@ -1,13 +1,19 @@
 import React from "react";
+import { withRouter } from "react-router-dom";
 
 import CollectionItem from "src/components/collection-item/CollectionItem";
 
 import "src/components/collection-preview/collectionpreview.styles.scss";
 
-const CollectionPreview = ({ title, items }) => {
+const CollectionPreview = ({ title, items, history, match }) => {
+  const navigateToCollection = () => {
+    history.push(`${match.url}/${title.toLowerCase()}`);
+  };
   return (
     <div className="collection-preview">
-      <h1 className="title">{title.toUpperCase()}</h1>
+      <h1 className="title" onClick={navigateToCollection}>
+        {title.toUpperCase()}
+      </h1>
       <div className="item-preview">
         {items
           .filter((_, index) => index < 4)
@@ -19,4 +25,4 @@ const CollectionPreview = ({ title, items }) => {
   );
 };
 
-export default CollectionPreview;
+export default withRouter(CollectionPreview);
