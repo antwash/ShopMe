9b82e21580bd40f114bf27437b494c6fd2cf7160
Anthony Washington <awashington251@gmail.com>
2021-02-11 09:23:55 -0500

    page: shop page component using loading spinner HOC

--- src/page/shop-page/ShopPage.jsx
+++ src/page/shop-page/ShopPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 import { connect } from "react-redux";
 import { Route } from "react-router-dom";
@@ -7,6 +7,7 @@ import { setShopItemsAction } from "src/redux/shop/shopActions";
 
 import CollectionPage from "src/page/collection-page/CollectionPage";
 import CollectionOverview from "src/components/collection-overview/CollectionOverview";
+import WithLoadingSpinner from "src/components/loading-spinner/loading-spinner";
 
 import {
   convertShopItemsCollectionSnapshotToMap,
@@ -14,22 +15,38 @@ import {
   fireStoreDbPaths,
 } from "src/firebase/util";
 
+const CollectionOverviewWithSpinner = WithLoadingSpinner(CollectionOverview);
+const CollectionPageWithSpinner = WithLoadingSpinner(CollectionPage);
+
 const ShopPage = ({ match, setShopItems }) => {
+  const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
     const collectionRef = firestore.collection(fireStoreDbPaths.shopItems);
     collectionRef.onSnapshot(async (snapshot) => {
       const shopItemsMap = convertShopItemsCollectionSnapshotToMap(snapshot);
       setShopItems(shopItemsMap);
+      setIsLoading(false);
     });
   });
 
   return (
     <div>
-      <Route exact path={`${match.path}`} component={CollectionOverview} />
+      <Route
+        exact
+        path={`${match.path}`}
+        render={(props) => {
+          return (
+            <CollectionOverviewWithSpinner isLoading={isLoading} {...props} />
+          );
+        }}
+      />
       <Route
         exact
         path={`${match.path}/:collectionId`}
-        component={CollectionPage}
+        render={(props) => {
+          return <CollectionPageWithSpinner isLoading={isLoading} {...props} />;
+        }}
       />
     </div>
   );
