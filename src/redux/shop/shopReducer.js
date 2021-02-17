4b468420301940729652b88b6365fbfc45cef0fc
Anthony Washington <awashington251@gmail.com>
2021-02-16 22:34:41 -0500

    shop: add async fetch shop items reducer

--- src/redux/shop/shopReducer.js
+++ src/redux/shop/shopReducer.js
@@ -1,15 +1,29 @@
 import { ShopActionTypes } from "src/redux/shop/shopActions";
 
 const INITIAL_STATE = {
-  collectionItems: [],
+  shopItems: [],
+  isFetchingShopItems: false,
+  errorMessage: "",
 };
 
 const shopReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case ShopActionTypes.setShopItems:
+    case ShopActionTypes.fetchShopItems:
       return {
         ...state,
-        collectionItems: action.payload,
+        isFetchingShopItems: true,
+      };
+    case ShopActionTypes.fetchShopItemsSuccess:
+      return {
+        ...state,
+        isFetchingShopItems: false,
+        shopItems: action.payload,
+      };
+    case ShopActionTypes.fetchShopItemsFailure:
+      return {
+        ...state,
+        isFetchingShopItems: false,
+        errorMessage: action.payload,
       };
     default:
       return state;
