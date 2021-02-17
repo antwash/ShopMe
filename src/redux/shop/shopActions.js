b02bf03dc928056ce94029944a7e5404b3a1be8f
Anthony Washington <awashington251@gmail.com>
2021-02-16 22:34:10 -0500

    shop: add async fetch shop items actions

--- src/redux/shop/shopActions.js
+++ src/redux/shop/shopActions.js
@@ -1,10 +1,40 @@
+import {
+  convertShopItemsCollectionSnapshotToMap,
+  fireStoreDbPaths,
+  firestore,
+} from "src/firebase/util";
+
 export const ShopActionTypes = {
-  setShopItems: "SET_SHOP_ITEMS",
+  fetchShopItems: "FETCH_SHOP_ITEMS",
+  fetchShopItemsSuccess: "FETCH_SHOP_ITEMS_SUCCESS",
+  fetchShopItemsFailure: "FETCH_SHOP_ITEMS_FAILURE",
 };
 
-export const setShopItemsAction = (shopItems) => {
-  return {
-    type: ShopActionTypes.setShopItems,
-    payload: shopItems,
+export const fetchShopItems = () => ({
+  type: ShopActionTypes.fetchShopItems,
+});
+
+export const fetchShopItemsSuccess = (shopItems) => ({
+  type: ShopActionTypes.fetchShopItemsSuccess,
+  payload: shopItems,
+});
+
+export const fetchShopItemsFailure = (errorMessage) => ({
+  type: ShopActionTypes.fetchShopItemsFailure,
+  errorMessage: errorMessage,
+});
+
+export const fetchShopItemsAsync = () => {
+  return (dispatch) => {
+    dispatch(fetchShopItems());
+
+    const collectionRef = firestore.collection(fireStoreDbPaths.shopItems);
+    collectionRef
+      .get()
+      .then((snapshot) => {
+        const collectionMap = convertShopItemsCollectionSnapshotToMap(snapshot);
+        dispatch(fetchShopItemsSuccess(collectionMap));
+      })
+      .catch((error) => dispatch(fetchShopItemsFailure(error.message)));
   };
 };
