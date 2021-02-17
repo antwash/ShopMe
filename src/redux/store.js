564396dafa68f3f332c1a222bf7835ea0f8a780b
Anthony Washington <awashington251@gmail.com>
2021-02-16 22:33:00 -0500

    store: apply redux-thunk middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,10 +2,11 @@ import { createStore, applyMiddleware } from "redux";
 
 import { persistStore } from "redux-persist";
 import logger from "redux-logger";
+import thunk from "redux-thunk";
 
 import rootReducer from "src/redux/rootReducer";
 
-const middleWares = [];
+const middleWares = [thunk];
 if (process.env.NODE_ENV === "development") {
   middleWares.push(logger);
 }
